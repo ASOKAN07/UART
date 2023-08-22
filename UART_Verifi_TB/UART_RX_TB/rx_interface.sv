65ee303cccfd4effd399e8365f86e1a6448a9a32
ASOKAN07 <140265974+ASOKAN07@users.noreply.github.com>
2023-08-22 08:16:04 +0530

    Update rx_interface.sv

--- UART_Verifi_TB/UART_RX_TB/rx_interface.sv
+++ UART_Verifi_TB/UART_RX_TB/rx_interface.sv
@@ -1,4 +1,3 @@
-
 interface UART_IF(input bit clk,baud_clk_tx);
   logic reset;
   logic rd_en;
@@ -8,12 +7,12 @@ interface UART_IF(input bit clk,baud_clk_tx);
   
   clocking WRD_RX_CB@(posedge baud_clk_tx);
   default input #1 output #1;
-   output rx;
+     output rx;
   endclocking 
   
   clocking WRM_RX_CB@(negedge baud_clk_tx);
   default input #1 output #1;
-  input rx;
+    input rx;
   endclocking 
 
   clocking RDM_RX_CB@(posedge clk);
@@ -26,15 +25,11 @@ interface UART_IF(input bit clk,baud_clk_tx);
   default input #1 output #1;
     input rx_empty;
     output rd_en;
-   
   endclocking
-  
   
   modport WRD_RX_M(clocking WRD_RX_CB);
   modport WRM_RX_M(clocking WRM_RX_CB);
   modport RDM_RX_M(clocking RDM_RX_CB);
   modport RDD_RX_M(clocking RDD_RX_CB);   
-   
-  
-
+     
 endinterface 
