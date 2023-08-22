edb49908656ce1588baf3b4a7a20056bed8f5ffa
ASOKAN07 <140265974+ASOKAN07@users.noreply.github.com>
2023-08-22 08:10:02 +0530

    Update rx_even_parity_test.sv

--- UART_Verifi_TB/UART_RX_TB/rx_even_parity_test.sv
+++ UART_Verifi_TB/UART_RX_TB/rx_even_parity_test.sv
@@ -2,24 +2,20 @@ int rx_no_of_transaction = 100;
 int rx_no_of_read_trans=0;
 int rx_no_of_write_trans=0;
 
-//`define ODD_PARITY
 `define EVEN_PARITY
 `include "rx_environment.sv"
 program  test_rx(UART_IF DUT_IF,input clk);  
-  
 
- class my_trans extends transaction;
-    bit [1:0] count;
-    function void pre_randomize();
-      bad_parity_bit.constraint_mode(0);
-      count_odd.constraint_mode(0);
-      count_even.constraint_mode(1);
-      good_parity_bit.constraint_mode(1);
-    endfunction
+class my_trans extends transaction;
+bit [1:0] count;
+  function void pre_randomize();
+  bad_parity_bit.constraint_mode(0);
+  count_odd.constraint_mode(0);
+  count_even.constraint_mode(1);
+  good_parity_bit.constraint_mode(1);
+  endfunction
+endclass
     
-  endclass
-  
-  
 environment env_h;
 my_trans my_tr;
   
@@ -30,11 +26,6 @@ initial begin
   my_tr=new();
   $display("-------------Even Parity Data Generating---------------");
   env_h.rx_gen_h.trans =my_tr;
-  //env_h.rx_covgrp_h.RX_coverage.DATA.option.weight=0;
-  //env_h.rx_covgrp_h.RX_coverage.DATA.option.weight=1'b0;
-  //env_h.rx_covgrp_h.RX_coverage.PARITY_RX_0.option.weight=0;
-  //env_h.rx_covgrp_h.RX_coverage.START_RX.option.weight=0;
-  //env_h.rx_covgrp_h.RX_coverage.STOP_RX.option.weight=0;
   env_h.run();
 end 
   
@@ -47,27 +38,4 @@ end
   endtask
 endprogram 
 
-/*
-class test;
-  virtual UART_IF.WRD_RX_M WRD_IF;
-  virtual UART_IF.WRM_RX_M WRM_IF;
-  virtual UART_IF.RDM_RX_M RDM_IF;
-  virtual UART_IF.RDD_RX_M RDD_IF;
-  environment env_h;
-  function new(virtual UART_IF.WRD_RX_M WRD_IF,
-  virtual UART_IF.WRM_RX_M WRM_IF,
-  virtual UART_IF.RDM_RX_M RDM_IF,  virtual UART_IF.RDD_RX_M RDD_IF);
- 
-    this.WRD_IF=WRD_IF;
-    this.WRM_IF=WRM_IF;
-    this.RDM_IF=RDM_IF;
-    this.RDD_IF=RDD_IF;
-    env_h=new(WRD_IF,WRM_IF,RDM_IF,RDD_IF);
-  endfunction 
-  
-  task build_and_run;
-  env_h.build();
-  env_h.run();
-  endtask 
-  
-endclass*/
+
