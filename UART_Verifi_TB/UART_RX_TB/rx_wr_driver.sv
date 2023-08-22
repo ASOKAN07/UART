5f92063e7e1e6d1b19ae701567df03e58f9345b4
ASOKAN07 <140265974+ASOKAN07@users.noreply.github.com>
2023-08-22 08:22:40 +0530

    Update rx_wr_driver.sv

--- UART_Verifi_TB/UART_RX_TB/rx_wr_driver.sv
+++ UART_Verifi_TB/UART_RX_TB/rx_wr_driver.sv
@@ -1,37 +1,31 @@
 class rx_wr_driver;
-  transaction trans,t1;
-  virtual UART_IF.WRD_RX_M WRD_IF;
-  mailbox gen2rx_drv;
 
-  function new(virtual UART_IF.WRD_RX_M WRD_IF,mailbox gen2rx_drv);
+transaction trans,t1;
+virtual UART_IF.WRD_RX_M WRD_IF;
+mailbox gen2rx_drv;
+
+function new(virtual UART_IF.WRD_RX_M WRD_IF,mailbox gen2rx_drv);
     this.gen2rx_drv=gen2rx_drv;
     this.WRD_IF=WRD_IF;
     $display("UART RX write Driver is Build");
- endfunction 
+endfunction 
   
-  virtual task drive();
-    repeat(11)  begin
-      
-      @(WRD_IF.WRD_RX_CB)begin
-     //$display("  befor Data is driveing  %b",trans.rx_reg[10]);
-      WRD_IF.WRD_RX_CB.rx<=trans.rx_reg[10];
-      trans.rx_reg=trans.rx_reg<<1;
-     //$display($time,"  Data is driveing  %b",trans.rx_reg[10]);
-     end   
-    end
+virtual task drive();
+  repeat(11)  begin
+    @(WRD_IF.WRD_RX_CB)begin
+    WRD_IF.WRD_RX_CB.rx<=trans.rx_reg[10];
+    trans.rx_reg=trans.rx_reg<<1;
+   end end
 WRD_IF.WRD_RX_CB.rx<=1'b1;
-  endtask
+endtask
   
-  virtual task start();
+virtual task start();
 fork 
- 	 forever   
-  	 begin  
-  	 gen2rx_drv.get(trans); 
-       //$display("  Data is driveing  %h",trans.rx_reg);
-       drive();
-       #100;
-  	 end
-  join_none
- 
-  endtask
+forever   begin  
+  gen2rx_drv.get(trans); 
+  drive(); #100; 
+end
+join_none
+endtask
+
 endclass;
