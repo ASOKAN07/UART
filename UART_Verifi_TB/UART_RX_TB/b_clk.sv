2fef6936be9559b0dd2f379aa8effdc9080b59ae
ASOKAN07 <140265974+ASOKAN07@users.noreply.github.com>
2023-08-23 13:09:42 +0530

    Update b_clk.sv

--- UART_Verifi_TB/UART_RX_TB/b_clk.sv
+++ UART_Verifi_TB/UART_RX_TB/b_clk.sv
@@ -4,12 +4,14 @@ input logic [10:0]dvsr;
 output logic baud_trig_tx,baud_trig_rx;
 reg [10:0]b_reg_tx,b_next_tx;
 reg [10:0]b_reg_rx,b_next_rx;
+
 always_ff@(posedge clk, posedge rst) begin 
 if(rst==1'b1)
 b_reg_tx<=1'b0;
 else
 b_reg_tx<=b_next_tx;
 end
+  
 always_ff@(negedge clk, posedge rst) begin 
 if(rst==1'b1)
 b_reg_rx<=1'b0;
@@ -19,6 +21,7 @@ end
 
 assign b_next_rx=(b_reg_rx==dvsr)?1:b_reg_rx+1;
 assign baud_trig_rx= (b_reg_rx==11'd2);
+  
 assign b_next_tx=(b_reg_tx==dvsr)?1:b_reg_tx+1;
 assign baud_trig_tx= (b_reg_tx==11'd1);
 endmodule 
