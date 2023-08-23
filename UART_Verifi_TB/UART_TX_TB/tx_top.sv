5be19d589578e297f0a00e4ac96eec844490ad75
ASOKAN07 <140265974+ASOKAN07@users.noreply.github.com>
2023-08-23 12:54:30 +0530

    tx_top.sv

--- UART_Verifi_TB/UART_TX_TB/tx_top.sv
+++ UART_Verifi_TB/UART_TX_TB/tx_top.sv
@@ -1,26 +1,21 @@
-`include "interface.sv"
+`include "tx_interface.sv"
 
-//Particular test case can be run by uncommenting and commenting on the rest
+//Particular testcase can be run by uncommenting, and commenting the rest
 
- `include "odd_parity_test.sv"
-//  `include "even_parity_test.sv"
-//  `include "random_test.sv"
-//  `include "toggle_data_test.sv"
-//  `include "range_def_test.sv"
+//`include "tx_even_parity_0_test.sv"
+//`include "tx_even_parity_1_test.sv"
+`include "tx_random_test.sv"
 
-module UART_TX_TB(); 
+module UART_TX_TB();
+parameter time_period=10;
+bit clk;
+always #(time_period/2) clk=~clk; 
 
- parameter time_period=10;
- bit clk;
- always #(time_period/2) clk=~clk; 
-
- UART_IF DUT_IF(clk);
- uart_top dut(clk,DUT_IF.reset,DUT_IF.wr_en,DUT_IF.d_in,DUT_IF.tx_full,DUT_IF.tx);
- test_tx t1(DUT_IF);
+UART_IF DUT_IF(clk);
+uart_top dut(clk,DUT_IF.reset,DUT_IF.wr_en,DUT_IF.d_in,DUT_IF.tx_full,DUT_IF.tx);
+test_tx t1(DUT_IF);
  
-  initial begin
-    $dumpfile("dump.vcd");
-    $dumpvars;
- end
+initial begin
+  $dumpfile("dump.vcd"); $dumpvars;
+end 
 endmodule
-
